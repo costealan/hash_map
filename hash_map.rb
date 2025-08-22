d5086ef144eceb428f1d9ae13163d3afd219dce2
cookingnoob <{ID}+{username}@users.noreply.github.com>
2025-08-22 14:34:10 -0600

    Add test data to HashMap and change initialize method to not include parameters

--- hash_map.rb
+++ hash_map.rb
@@ -1,8 +1,8 @@
 require_relative 'linked_list'
 class HashMap
   attr_accessor :capacity
-  def initialize(load_factor)
-    @load_factor = load_factor
+  def initialize
+    @load_factor = 0.75
     @capacity = Array.new(16)
   end
 
@@ -51,16 +51,40 @@ class HashMap
 
   def remove(key)
     index = hash(key)
+    if @capacity[index] == nil
+      return nil
+    end
     if !@capacity[index].contains?(key)
       return nil
     end
+   removed_node = @capacity[index].find_node(key)
    @capacity[index].remove_at(@capacity[index].find_index(key))
+   removed_node
+  end
+
+  def length
+    #loops through buckets
+    # for each different key in the bucket
   end
 end
 
-hash_map = HashMap.new(0.75)
-hash_map.set('Alan', 'Costa')
-hash_map.set('Alan', 'Coste')
-hash_map.set('Coffee', 'Nespresso')
-hash_map.remove('Coffee')
-p hash_map.capacity
+ test = HashMap.new
+
+ test.set('apple', 'red')
+ test.set('banana', 'yellow')
+ test.set('carrot', 'orange')
+ test.set('dog', 'brown')
+ test.set('elephant', 'gray')
+ test.set('frog', 'green')
+ test.set('grape', 'purple')
+ test.set('hat', 'black')
+ test.set('ice cream', 'white')
+ test.set('jacket', 'blue')
+ test.set('kite', 'pink')
+ test.set('lion', 'golden')
+
+ test.set('lion', 'dorado')
+ test.set('frog', 'verde')
+ test.set('elephant', 'gris')
+
+ p test.capacity
