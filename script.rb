aababb56f5bd9c65daa11528f6e8ee24a09731ad
cookingnoob <{ID}+{username}@users.noreply.github.com>
2025-08-22 09:51:50 -0600

    add set method
    Obtains a number by hashing the key, using that number as the array in capacity. At the moment it stores the value

--- script.rb
+++ script.rb
@@ -11,8 +11,15 @@ class HashMap
       
    key.each_char { |char| hash_code = prime_number * hash_code + char.ord }
       
-   hash_code
+   hash_code % 16
+  end
+
+  def set(key, value)
+    hash_code = hash(key)
+    @capacity[hash_code] = value
   end
 end
 
 hash_map = HashMap.new(0.75)
+hash_map.set('Alan', 'Coste')
+p hash_map.capacity
