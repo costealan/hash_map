327fcccb731836e18805135db836476ad67593e2
cookingnoob <{ID}+{username}@users.noreply.github.com>
2025-08-21 18:51:38 -0600

    add hash method
    Create hash code by multiplying each characters and previous with a prime number

--- script.rb
+++ script.rb
@@ -3,4 +3,13 @@ class HashMap
     @load_factor = nil
     @capacity = nil
   end
+
+  def hash(key)
+   hash_code = 0
+   prime_number = 31
+      
+   key.each_char { |char| hash_code = prime_number * hash_code + char.ord }
+      
+   hash_code
+  end
 end
